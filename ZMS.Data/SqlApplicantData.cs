eec9c6cd0b04c0021182f31e19abd8135679558c
Mitchell Scott <mitchellscott@me.com>
2020-10-27 12:55:47 -0500

    set up files for EF Core

---
+++ ZMS.Data/SqlApplicantData.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ZMS.Domain;
+
+namespace ZMS.Data
+{
+    public class SqlApplicantData : IApplicantData
+    {
+        public SqlApplicantData(ApplicantDbContext db)
+        {
+
+        }
+
+        public Applicant Add(Applicant newApplicant)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Conversation AddConversation(Conversation newConversation)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Commit()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Applicant Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Conversation DeleteConversation(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Applicant> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Applicant GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Applicant> GetByTraining()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Conversation GetConversationById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Conversation> GetConversations(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Applicant Update(Applicant updatedApplicant)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Conversation UpdateConversation(Conversation updatedConversation)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- ZMS/Startup.cs
+++ ZMS/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -23,8 +24,12 @@ namespace ZMS
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddSingleton<IApplicantData, InMemoryApplicantData>();
+        {   // enable below for in memory testing, plug in the data service file
+            //services.AddSingleton<IApplicantData, InMemoryApplicantData>();
+
+            //enable below for sql server
+            services.AddDbContextPool<ApplicantDbContext>(options => { options.UseSqlServer(Configuration.GetConnectionString("ZMSDb")); });
+
             services.AddRazorPages();
         }
 
