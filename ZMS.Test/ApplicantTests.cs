2b5a2724fa799c95396055aea62b6f0c8482fcae
Mitchell Scott <mitchellscott@me.com>
2020-10-28 15:50:25 -0500

    added partial scripts validation for client side

---
+++ ZMS.Test/ApplicantTests.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using Xunit;
+using ZMS.Domain;
+using System.Linq;
+using ZMS.Data;
+
+namespace ZMS.Test
+{
+    public class ApplicantTests
+    {
+        
+        /*[Fact]
+        public void CanAddAnApplicant()
+        {
+        }*/
+
+           /* [Fact]
+        public void CanGetApplicantById()
+        {
+          
+        }*/
+    }
+}
